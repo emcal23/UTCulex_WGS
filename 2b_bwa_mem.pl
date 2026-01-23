fc659c1db7119d6bdc36d2327c1ea79738132292
Emily Grace <100314761+emcal23@users.noreply.github.com>
2026-01-23 13:26:50 -0700

    Update 2b_bwa_mem.pl

--- 2b_bwa_mem.pl
+++ 2b_bwa_mem.pl
@@ -14,7 +14,8 @@ my $genome = "/uufs/chpc.utah.edu/common/home/u1055819/saarman-group/cx_ddRAD_bw
 my $output_dir = "/uufs/chpc.utah.edu/common/home/u1055819/saarman-group/Cx_WGS/Cx_WGS_bwa";
 
 # Path to bwa-mem2 binary
-my $bwa = "bwa-mem2"; #module loaded in SLURM
+my $bwa = "/uufs/chpc.utah.edu/sys/installdir/r8/bwa-mem/2.2.1-smem_alloc/bwa-mem2.avx2";
+#my $bwa = "bwa-mem2"; #module loaded in SLURM
 #my $bwa = "/uufs/chpc.utah.edu/sys/installdir/bwa/2.2.1/bin/bwa";
 
 # Path to samtools
